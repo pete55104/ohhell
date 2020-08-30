f2d52a00fee01f1348f294d8b9119fd16f1cb190
Pete Gaffney <petertgaffney@gmail.com>
2020-08-30 09:48:53 -0500

    fixed service

--- server/handlers.js
+++ server/handlers.js
@@ -2,7 +2,7 @@ const AWS = require('aws-sdk');
 
 const ddb = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10', region: process.env.AWS_REGION });
 
-exports.onconnect = async event => {
+exports.connectHandler = async event => {
   const putParams = {
     TableName: process.env.TABLE_NAME,
     Item: {
@@ -19,7 +19,7 @@ exports.onconnect = async event => {
   return { statusCode: 200, body: 'Connected.' };
 };
 
-exports.ondisconnect = async event => {
+exports.disconnectHandler = async event => {
     const deleteParams = {
       TableName: process.env.TABLE_NAME,
       Key: {
@@ -36,29 +36,31 @@ exports.ondisconnect = async event => {
     return { statusCode: 200, body: 'Disconnected.' };
 };
 
-exports.default = async event => {
+exports.defaultHandler = async event => {
     let connectionData;
-    
+    console.log(`defaultHandler entry with ${process.env.TABLE_NAME} and ddb client${!!ddb}`)
     try {
-        connectionData = await ddb.scan({ TableName: TABLE_NAME, ProjectionExpression: 'connectionId' }).promise();
+        connectionData = await ddb.scan({ TableName: process.env.TABLE_NAME, ProjectionExpression: 'connectionId' }).promise();
     } catch (e) {
+        console.log(`defaultHandler error: ${JSON.stringify(e)}`)
         return { statusCode: 500, body: e.stack };
     }
-    
+    console.log('defaultHandler connectionData: ', JSON.stringify(connectionData))
     const apigwManagementApi = new AWS.ApiGatewayManagementApi({
         apiVersion: '2018-11-29',
         endpoint: event.requestContext.domainName + '/' + event.requestContext.stage
     });
     
     const postData = JSON.parse(event.body).data;
-    
+    console.log('defaultHandler postData: ', JSON.stringify(postData))
     const postCalls = connectionData.Items.map(async ({ connectionId }) => {
         try {
         await apigwManagementApi.postToConnection({ ConnectionId: connectionId, Data: postData }).promise();
+        console.log('defaultHandler: postedToConnection')
         } catch (e) {
         if (e.statusCode === 410) {
             console.log(`Found stale connection, deleting ${connectionId}`);
-            await ddb.delete({ TableName: TABLE_NAME, Key: { connectionId } }).promise();
+            await ddb.delete({ TableName: process.env.TABLE_NAME, Key: { connectionId } }).promise();
         } else {
             throw e;
         }
@@ -67,6 +69,7 @@ exports.default = async event => {
     
     try {
         await Promise.all(postCalls);
+        console.log('defaultHandler: postCalls')
     } catch (e) {
         return { statusCode: 500, body: e.stack };
     }
