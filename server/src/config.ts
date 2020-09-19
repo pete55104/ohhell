9f1abe1f24b0aafe7f286f107354a78f586a70d2
Pete Gaffney <petertgaffney@gmail.com>
2020-09-19 13:42:45 -0500

    OH010 setup configs

--- server/src/config.ts
+++ server/src/config.ts
@@ -6,8 +6,30 @@ function stringOrThrow(variableName: string): string {
     throw new Error(`${variableName} must be defined as string`)
 }
 
+const requiredStringEnvVariables: { [key: string]: string; } = {
+    tableName: "TABLE_NAME"
+}
+
+// enum optionalStringEnvVariables {
+// }
+
+const missing = Object.keys(requiredStringEnvVariables).reduce(
+    (missingList: Array<string>, next: string) => {
+        if(!process.env[requiredStringEnvVariables[next]]){
+            missingList.push(requiredStringEnvVariables[next])
+        }
+        return missingList
+    }, []
+)
+if(missing.length){
+    const err = `These env variables must be defined as strings: ${missing.join(',')}`
+    console.error(err)
+    throw new Error(err)
+}
+
+
 const config = {
-    tableName: stringOrThrow("TABLE_NAME")
+    tableName: stringOrThrow(requiredStringEnvVariables.tableName)
 }
 
 export default config
