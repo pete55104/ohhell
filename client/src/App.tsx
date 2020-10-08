7ac84c39bf74705863609a3e2854bf46e4a40a9a
Pete Gaffney <petertgaffney@gmail.com>
2020-10-07 23:34:05 -0500

    OH019 top menu

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,41 +1,33 @@
-import React, {Component, FormEvent} from 'react'
-import { Route, Switch, Redirect } from "react-router-dom"
+import React, {Component} from 'react'
+import { Route, Switch, Redirect, NavLink } from "react-router-dom"
 import './App.css'
 import Echo from "./components/Echo"
+import Lobby from "./components/Lobby"
 import SleepingBear from "./components/SleepingBear"
 import Sample from "./components/Sample"
 import Nothing from "./components/Nothing"
-import {w3cwebsocket as W3CWebSocket} from "websocket"
 
-const url = 'wss://ncqq73m9x7.execute-api.us-east-1.amazonaws.com/dev'
-const client = new W3CWebSocket(url)
-
-export interface ICustomAppState {
-    data?: string;
-    timeStampSent?: number;
-    timeStampReceived?: number;
-    origin?: string;
-    isTrusted?: boolean;
-}
-
-class App extends Component<{}, ICustomAppState> {
-
-    constructor(props: any) {
-        super(props);
-    }
-
+class App extends Component<{}, {}> {
     render() {
         return (
-                <div className="App">
-                    <Switch>
-                        <Route path="/" exact component={Echo} />
-                        <Route path="/sample" render={routerProps => <Sample {...routerProps} sampleProp={"sample"}/>} />
-                        <Route path="/sleeping-bear" component={SleepingBear} />
-                        <Route path='/default' render={() => <Redirect to= "/" />} />
-                        <Route component={Nothing} />
-                    </Switch>
-                </div> 
-            )
+        <div className="App">
+            <div className="link-container">
+                <NavLink activeClassName="active" exact to="/" style={{paddingLeft: "10px"}}>lobby</NavLink>
+                <NavLink activeClassName="active" to="/sample" style={{paddingLeft: "10px"}}>sample</NavLink>
+                <NavLink activeClassName="active" to="/nowhere" style={{paddingLeft: "10px"}}>nowhere</NavLink>
+                <NavLink activeClassName="active" to="/sleeping-bear" style={{paddingLeft: "10px"}}>a sleeping bear</NavLink>
+                <NavLink activeClassName="active" to="/echo" style={{paddingLeft: "10px"}}>echo</NavLink>
+            </div>
+            <Switch>
+                <Route path="/" exact component={Lobby} />
+                <Route path="/echo" component={Echo} />
+                <Route path="/sample" render={routerProps => <Sample {...routerProps} sampleProp={"sample"}/>} />
+                <Route path="/sleeping-bear" component={SleepingBear} />
+                <Route path='/default' render={() => <Redirect to= "/" />} />
+                <Route component={Nothing} />
+            </Switch>
+        </div>
+        )
     }
 }
 
