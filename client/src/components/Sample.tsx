a4ed7c13b5827d2c5ab1bbe3c6c228b5ed7db3e1
Pete Gaffney <petertgaffney@gmail.com>
2020-10-11 21:13:49 -0500

    Sample uses state and buttons

--- client/src/components/Sample.tsx
+++ client/src/components/Sample.tsx
@@ -1,11 +1,26 @@
-import React, { FC } from 'react'
+import React, { FC, ChangeEvent, useState } from 'react'
 import { NavLink, RouteComponentProps } from 'react-router-dom';
 
-interface SampleProps extends RouteComponentProps {
+interface IProps extends RouteComponentProps {
     sampleProp: string
 } 
 
-const Sample: FC<SampleProps> = ( props: SampleProps): JSX.Element => {
+interface IState{
+
+}
+
+const Sample: FC<IProps> = props => {
+    const [color, setColor] = useState("red");
+    const [truthiness, setTruthiness] = useState(true);
+
+    const toggleTruthiness = () => {
+        setTruthiness(!truthiness)
+    }
+
+    const handleColorChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setColor(event.currentTarget.value)
+    }
+
     console.log(props);
     return (
     <div className="centeringdiv">
@@ -13,6 +28,13 @@ const Sample: FC<SampleProps> = ( props: SampleProps): JSX.Element => {
     <div>
         <span>This is the {props.sampleProp} component route</span>
         <NavLink activeClassName="active" exact to="/">Redirect to /</NavLink>
+    <p>This page's truthiness is rated at {truthiness ? "true" : "false"} <button onClick={toggleTruthiness}>toggle truthiness</button></p>
+    <p>This page's color is rated at {color} 
+        <input 
+            type="text"
+            id="color"
+            value={color}
+            onChange={handleColorChange}/></p>
     </div>
     </div>
     );
