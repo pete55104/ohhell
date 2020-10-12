83226207a1980deb1f71420f66d509a0e2e832a0
Pete Gaffney <petertgaffney@gmail.com>
2020-10-11 20:44:07 -0500

    Sample component now uses hooks

--- client/src/components/Sample.tsx
+++ client/src/components/Sample.tsx
@@ -1,24 +1,21 @@
-import React, {Component } from 'react'
+import React, { FC } from 'react'
 import { NavLink, RouteComponentProps } from 'react-router-dom';
 
 interface SampleProps extends RouteComponentProps {
     sampleProp: string
 } 
 
-class Sample extends Component<SampleProps, {}> {
-
-    render() {
-        console.log(this.props);
-        return (
-        <div className="centeringdiv">
-            <h1>sample</h1>
-          <div>
-            <span>This is the {this.props.sampleProp} component route</span>
-            <NavLink activeClassName="active" exact to="/">Redirect to /</NavLink>
-          </div>
-          </div>
-        );
-    }
+const Sample: FC<SampleProps> = ( props: SampleProps): JSX.Element => {
+    console.log(props);
+    return (
+    <div className="centeringdiv">
+        <h1>sample</h1>
+    <div>
+        <span>This is the {props.sampleProp} component route</span>
+        <NavLink activeClassName="active" exact to="/">Redirect to /</NavLink>
+    </div>
+    </div>
+    );
 }
 
 export default Sample;
